3252394aab08622df2396eaf498c3bb68c60726c
Jjgod Jiang <gzjjgod@gmail.com>
2008-12-08 17:48:23 +0800

    A few new updates:
    1. Describe the format,
    2. Add synopsis support,
    3. Support comma separated See Also list.

--- dz.py
+++ dz.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 
-"""DocZilla: A documentation generator based on Apple documentation style."""
+"""DocZilla: A documentation generator based on Apple documentation style.
+
+DocZilla format:
+
+    @ Document Title (It will be show on the <title> and <h1>)
+
+    A short overview of the entire document. (Can use Markdown format, but
+    no header is allowed. So you can write multiple paragraphs, just don't
+    create new headers.)
+
+    # Entry Name (A short entry name, like the function name)
+    % AttrName: Attr Value (Attr Name can be 'in', 'sa', for "Declared In",
+      "See Also" correspondingly. For "sa", you can specify a comma separ-
+      ated list. For "in", it will be treated as a string. More than one
+      attributes are allowed.)
+
+    A short, one line synopsis, in Markdown format.
+
+        type func(params) (The function prototype, can be more than one
+                           line, but each line must be started with four
+                           spaces)
+
+    A longer, multi-line discussion, on the usage and return value. In
+    Markdown format.
+"""
 
 import os, sys, markdown2, tenjin
 from tenjin.helpers import *
 
-(GET_TITLE, GET_OVERVIEW, GET_NAME, GET_ATTR, GET_FUNC, GET_DESC) = range(6)
+(GET_TITLE, GET_OVERVIEW, GET_NAME, GET_ATTR, GET_SYNOPSIS, GET_FUNC, GET_DISCUSSION) = range(7)
 
 engine      = tenjin.Engine(encoding="utf-8")
 markdowner  = markdown2.Markdown()
@@ -41,35 +65,43 @@ for line in fin:
             keylen = line.find(":")
             key = line[1:keylen].strip()
             entry[key] = line[keylen + 1:].strip()
+            if key == "sa":
+                entry[key] = [ item.strip() for item in entry[key].split(",")]
+        else:
+            state = GET_SYNOPSIS
 
-        if line[0:4] == "    ":
-            entry["func"] = line[4:].strip()
-            state = GET_FUNC
+    elif state == GET_SYNOPSIS:
+        entry["synopsis"] = markdowner.convert(line.strip())
+        state = GET_FUNC
+        entry["func"] = ""
 
     elif state == GET_FUNC:
+        if len(entry["func"]) == 0 and line == "\n":
+            continue
+
         if line[0:4] == "    ":
-            entry["func"] += " " + line[4:].strip()
+            entry["func"] += line[4:].strip()
 
         else:
-            entry["desc"] = line.strip()
-            state = GET_DESC
+            entry["discussion"] = line.strip()
+            state = GET_DISCUSSION
 
-    elif state == GET_DESC:
+    elif state == GET_DISCUSSION:
         if line == "\n":
-            entry["desc"] = markdowner.convert(entry["desc"])
+            entry["discussion"] = markdowner.convert(entry["discussion"])
             entries.append(entry)
 
             entry = {}
             state = GET_NAME
         else:
-            entry["desc"] += (len(entry["desc"]) and " " or "") + line.strip()
+            entry["discussion"] += (len(entry["discussion"]) and " " or "") + line.strip()
 
     else:
         print("You shouldn't be here (state %d)." % state)
 
 if entry:
-    if entry["desc"]:
-        entry["desc"] = markdowner.convert(entry["desc"])
+    if entry["discussion"]:
+        entry["discussion"] = markdowner.convert(entry["discussion"])
     entries.append(entry)
 
 context = { "title": title, "overview": overview, "entries": entries }
